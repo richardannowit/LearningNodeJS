199a840749d90bccdf991db98b4c8813ac6174db
richardannowit <richardannowit@gmail.com>
2020-06-03 23:46:16 +0700

    Reconstruct the project

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 /**
  * @file  : index.js
  * @author: Richard Annowit <richardannowit@gmail.com>
- * Date   : 02-06-2020 20:22:18
+ * Date   : 03-06-2020 21:27:12
  */
+const express = require('express');
+const path = require('path');
+const app = new express();
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
 
-const http = require("http");
 
-const server = http.createServer((req,res)=>{
-    console.log(req.url);
-    res.end('Hello World');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.set('view engine','ejs');
+
+
+app.get('/',(req,res)=>{
+    res.render('index');
 });
-server.listen(3000);
+
+app.get('/about',(req,res)=>{
+    res.render('about');
+});
+app.get('/contact',(req,res)=>{
+    res.render('contact');
+});
+
+app.get('/post',(req,res)=>{
+    res.render('post');
+});
+
+app.get('/posts/new',(req,res)=>{
+    res.render('create');
+});
+
+app.post('/posts/store',(req,res)=>{
+	console.log(req.body);
+	res.redirect('/post');
+});
+
+app.listen(3000,()=>{
+    console.log("Listening in port 3000");
+});
+
+
